da751f01475b5b38c6445ba324fafaf8f32cfa54
Dmytro <samtvone@gmail.com>
2019-03-14 14:17:01 +0200

    preparation is over

---
+++ js/card.js
@@ -0,0 +1,42 @@
+class Card {
+  constructor(settings) {
+    this._isRed = settings.isRed;
+    this._suit = settings.suit;
+    this._cardValue = settings.cardValue;
+    this._value = settings.value;
+    this._element = document.createElement('div');
+
+    this.isOpen = false;
+
+    this._render();
+  }
+
+  getElement() {
+    return this._element;
+  }
+
+  getValue() {
+    return this._value;
+  }
+
+  open() {
+    this.isOpen = true;
+    this._element.children[0].classList.remove('card__back');
+    this._render;
+  }
+
+  _render() {
+    const classes = ['card'];
+
+    classes.push(this._isRed ? 'card__red' : 'card__black');
+
+    if (!this.isOpen) {
+      classes.push('card__back');
+    }
+
+    this._element.innerHTML = `
+    <div class='${classes.join(' ')}'>
+    ${this._cardValue} ${this._suit}
+    </div>`;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,60 +1,70 @@
-'use strict'
+'use strict';
 
 const cardSettings = {
   suits: ['♠', '♣', '♦', '♥'],
-  signs: ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-}
+  signs: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+};
 
-window.addEventListener('load', function () {
-  new Game()
-})
-
+// const stackNames = ['base', 'work1', 'work2', 'work3', 'work4', 'work5', 'work6', 'work7', 'fin1', 'fin2', 'fin3', 'fin4']
+const stack = {};
 class Game {
-  constructor () {
-    const deck = new Deck()
+  constructor() {
+    const cards = this._getCards();
+      
+    const stackBase = new Placeholder({
+      element: document.getElementById('stack_base'),
+      cards: cards.splice(0, 24),
+      isShifted: false,
+      onCardSelected: (cardsSelected) => {  }
+    });
 
-    const stackBase = new PlaceholderBase(document.getElementById('stack_base'))
-    stackBase._stack = stackBase._stack.concat(deck._deck.splice(0, 24))
-    stackBase._render()
+    // const stackBaseDrops = new PlaceholderBase(
+    //   document.getElementById('stack_base_r')
+    // );
 
-    const stackBaseDrops = new PlaceholderBase(document.getElementById('stack_base_r'))
+    for (let i = 1; i < 8; i++) {
+    stack[ 'num_'+ i ] = new Placeholder({
+      element: document.getElementById([ 'deck_' + i ]),
+      cards: cards.splice( 0, i ),
+      isShifted: true
+    });
+    }
 
-    const stack_1 = new Placeholder(document.getElementById('deck_1'))
-    stack_1._stack = stack_1._stack.concat(deck._deck.splice(0, 1))
-    stack_1._render()
+    for (let i = 1; i < 5; i++) {
+    stack[ 'final_'+ i ] = new Placeholder({
+      element: document.getElementById([ 'deck__final__' + i ]),
+      cards: [],
+      isShifted: false
+    });
+    }
 
-    const stack_2 = new Placeholder(document.getElementById('deck_2'))
-    stack_2._stack = stack_2._stack.concat(deck._deck.splice(0, 2))
-    stack_2._render()
 
-    const stack_3 = new Placeholder(document.getElementById('deck_3'))
-    stack_3._stack = stack_3._stack.concat(deck._deck.splice(0, 3))
-    stack_3._render()
-
-    const stack_4 = new Placeholder(document.getElementById('deck_4'))
-    stack_4._stack = stack_4._stack.concat(deck._deck.splice(0, 4))
-    stack_4._render()
-
-    const stack_5 = new Placeholder(document.getElementById('deck_5'))
-    stack_5._stack = stack_5._stack.concat(deck._deck.splice(0, 5))
-    stack_5._render()
-
-    const stack_6 = new Placeholder(document.getElementById('deck_6'))
-    stack_6._stack = stack_6._stack.concat(deck._deck.splice(0, 6))
-    stack_6._render()
-
-    const stack_7 = new Placeholder(document.getElementById('deck_7'))
-    stack_7._stack = stack_7._stack.concat(deck._deck.splice(0, 7))
-    stack_7._render()
-
-    const game = document.querySelector('#game')
-
-    game.addEventListener('click', (event) => {
-        this._render();
-     
-    })
+    const game = document.querySelector('#game');
+    game.addEventListener('click', event => {
+      console.log(event.target);
+    });
   }
-  _render () {
-    //   alert('1');
+
+  _getCards() {
+    const baseDeck = [];
+
+    for (let i = 0; i < cardSettings.suits.length; i++) {
+      let value = 1; 
+      for (let cardValue of cardSettings.signs) {
+        let cardCreate = new Card({ value, cardValue, suit: cardSettings.suits[i], isRed: i > 1 });
+        baseDeck.push(cardCreate);
+        value++;
+      }
+    }
+    // let i = baseDeck.length - 1;
+    // while (i >= 0) {
+    //   let index = Math.round(Math.random() * i);
+    //   cards.push(baseDeck.splice(index, 1)[0]);
+    //   i--;
+    // }
+    return baseDeck.sort((a, b) => Math.random() - 0.5);
   }
 }
+
+
+new Game();
