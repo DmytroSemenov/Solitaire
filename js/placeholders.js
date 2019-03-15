7bf49e9a606a205576ce8601674be6c7da102810
DmytroSemenov <samtvone@gmail.com>
2019-03-15 18:15:18 +0200

    Update placeholders.js

--- js/placeholders.js
+++ js/placeholders.js
@@ -6,8 +6,7 @@ class Placeholder {
   // this._render()
   }
   _render () {
-
-    this._element.innerHTML = '';
+    this._element.innerHTML = ''
 
     if (this._stack.length) {
       for (let i = 0; i < this._stack.length; i++) {
@@ -16,7 +15,7 @@ class Placeholder {
         elementShifted.style.marginTop = marginTopY + 'px'
 
         if (i === this._stack.length - 1) {
-          this._element.appendChild(this._stack[i]._elementCard)
+          this._element.appendChild(this._stack[i]._elementCard);
         } else {
           if (!this._stack[i].isOpen) this._stack[i]._elementCard.firstChild.classList.add('card__back')
           this._element.appendChild(elementShifted)
